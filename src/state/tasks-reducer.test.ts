da46a875dc68f38475712b7dfdaf26bed031517a
kozincov <kozincov1998@gmail.com>
2022-04-10 22:10:27 +0300

    create task-reducer & test for added task

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -1,4 +1,4 @@
-import {removeTaskAC, TasksReducer} from './tasks-reducer';
+import {addTaskAC, removeTaskAC, TasksReducer} from './tasks-reducer';
 import {TasksStateType} from '../App';
 
 test('correct task should be deleted from correct array', () => {
@@ -29,4 +29,27 @@ test('correct task should be deleted from correct array', () => {
         ]
     });
 
+});
+
+test('correct task should be added to correct array', () => {
+    const startState: TasksStateType = {
+        "todoListId1": [
+            { id: "1", title: "CSS", isDone: false },
+            { id: "2", title: "JS", isDone: true },
+            { id: "3", title: "React", isDone: false }
+        ],
+        "todoListId2": [
+            { id: "1", title: "bread", isDone: false },
+            { id: "2", title: "milk", isDone: true },
+            { id: "3", title: "tea", isDone: false }
+        ]
+    };
+
+    const endState = TasksReducer(startState, addTaskAC("todoListId2", "beer"))
+
+    expect(endState["todoListId1"].length).toBe(3);
+    expect(endState["todoListId2"].length).toBe(4);
+    expect(endState["todoListId2"][0].id).toBeDefined();
+    expect(endState["todoListId2"][0].title).toBe("beer");
+    expect(endState["todoListId2"][0].isDone).toBe(false);
 });
--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,4 +1,5 @@
 import {TasksStateType} from "../App";
+import {v1} from "uuid";
 
 export const TasksReducer = (state: TasksStateType, action: GeneralTypeForAC) => {
     switch (action.type) {
@@ -7,12 +8,19 @@ export const TasksReducer = (state: TasksStateType, action: GeneralTypeForAC) =>
                 ...state,
                 [action.payload.todoListId]: state[action.payload.todoListId].filter(f => f.id !== action.payload.taskId)
             }
+        case "ADD-TASK":
+            let newTask = {id: v1(), title: action.payload.title, isDone: false}
+            return {
+                ...state,
+                [action.payload.todoListId]: [newTask, ...state[action.payload.todoListId]]
+            }
         default:
             return state
     }
 };
 
 export type GeneralTypeForAC = removeTaskACType
+    | addTaskACType
 
 export const removeTaskAC = (todoListId: string, taskId: string) => {
     return {
@@ -25,3 +33,15 @@ export const removeTaskAC = (todoListId: string, taskId: string) => {
 }
 
 export type removeTaskACType = ReturnType<typeof removeTaskAC>
+
+export const addTaskAC = (todoListId: string, title: string) => {
+    return {
+        type: 'ADD-TASK',
+        payload: {
+            todoListId,
+            title
+        }
+    } as const
+}
+
+export type addTaskACType = ReturnType<typeof addTaskAC>
