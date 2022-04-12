86ee9eb8315f19042d9436f8973d7a4d6bccd922
kozincov <kozincov1998@gmail.com>
2022-04-12 14:26:12 +0300

    replacing App with AppWithReducers

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {App} from './App';
+import {AppWithReducers} from "./AppWithReducers";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <AppWithReducers/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
