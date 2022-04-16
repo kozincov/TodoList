0a2b3eb945e5fe392fae271bdaad352664c418d1
kozincov <kozincov1998@gmail.com>
2022-04-16 22:00:36 +0300

    create stories for CreateTodoLists

--- src/stories/todoLists-api.stories.tsx
+++ src/stories/todoLists-api.stories.tsx
@@ -6,13 +6,28 @@ export default {
 }
 
 const settings = {
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'API-KEY': 'a8f18ed9-cd22-42f9-b24e-d66a98812746'
+    }
 }
 
 export const GetTodoLists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
         axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+            .then(res => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const CreateTodoLists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', { title: 'React 18' }, settings)
             .then(res => {
                 setState(res.data)
             })
