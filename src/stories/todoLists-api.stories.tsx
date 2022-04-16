e6106d7f08c023a84d51609ee99f62cef7207179
kozincov <kozincov1998@gmail.com>
2022-04-16 21:53:22 +0300

    create stories for getTodoLists

---
+++ src/stories/todoLists-api.stories.tsx
@@ -0,0 +1,22 @@
+import React, {useEffect, useState} from 'react'
+import axios from "axios";
+
+export default {
+    title: 'API'
+}
+
+const settings = {
+    withCredentials: true
+}
+
+export const GetTodoLists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+            .then(res => {
+                setState(res.data)
+            })
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
