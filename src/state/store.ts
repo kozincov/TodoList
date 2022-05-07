4dc8c22e11eab227b2bde4990e75c5d1da702422
kozincov <kozincov1998@gmail.com>
2022-05-07 20:49:05 +0300

    added applyMiddleware

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,7 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {TasksReducer} from "./tasks-reducer";
 import {todoListsReducer} from "./todo-lists-reducer";
+import thunk from "redux-thunk";
 
 
 const rootReducer = combineReducers({
@@ -8,7 +9,7 @@ const rootReducer = combineReducers({
     todoLists: todoListsReducer
 })
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
